1b6bfc9b33068b62ab8feebf87f7f520ca5d01fb
ivan <ivanvaneck2000@gmail.com>
2025-05-18 04:50:24 +0300

    remove secure route

--- src/main.rs
+++ src/main.rs
@@ -20,24 +20,9 @@ enum Route {
     Home,
     #[at("/boards/:slug")]
     Boards { slug: String },
-    #[at("/secure")]
-    Secure,
     #[not_found]
     #[at("/404")]
     NotFound,
-}
-
-#[function_component(Secure)]
-fn secure() -> Html {
-    let navigator = use_navigator().unwrap();
-
-    let onclick = Callback::from(move |_| navigator.push(&Route::Home));
-    html! {
-        <div>
-            <h1>{ "Secure" }</h1>
-            <button {onclick}>{ "Go Home" }</button>
-        </div>
-    }
 }
 
 #[derive(Properties, PartialEq)]
@@ -75,9 +60,6 @@ fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <App /> },
         Route::Boards { slug } => html! { <BoardPage slug={slug} /> },
-        Route::Secure => html! {
-            <Secure />
-        },
         Route::NotFound => html! { <h1>{ "404" }</h1> },
     }
 }

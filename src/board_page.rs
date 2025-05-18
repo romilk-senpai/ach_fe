a56d90621e358b07e18ffee6092bd0e199175501
ivan <ivanvaneck2000@gmail.com>
2025-05-18 06:07:08 +0300

    PostingForm

--- src/board_page.rs
+++ src/board_page.rs
@@ -1,9 +1,51 @@
 use crate::create_urbit_name::create_urbit_name;
-use crate::types::Thread;
+use crate::types::{BoardExt, Thread};
 use crate::use_fetch_board::use_fetch_board;
 use crate::BoardsList;
 use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
 use yew::prelude::*;
+
+#[derive(Properties, PartialEq)]
+struct PostingFormProps {
+    board: BoardExt,
+}
+
+#[function_component(PostingForm)]
+fn posting_form(PostingFormProps { board }: &PostingFormProps) -> Html {
+    let show_form = use_state(|| false);
+
+    if *show_form {
+        html! {
+            <form class="posting-form">
+                <div class="form-group">
+                    <label for="name">{"Name"}</label>
+                    <input type="text" id="name" name="name" value="Anonymous" />
+                </div>
+                <div class="form-group">
+                    <label for="options">{"Options"}</label>
+                    <input type="text" id="options" name="options" />
+                </div>
+                <div class="form-group">
+                    <label for="subject">{"Subject"}</label>
+                    <input type="text" id="subject" name="subject" />
+                </div>
+                <div class="form-group">
+                    <label for="comment">{"Comment"}</label>
+                    <textarea id="comment" name="comment" rows="5" />
+                </div>
+                <div class="form-group">
+                    <label for="file">{"File"}</label>
+                    <input type="file" id="file" name="file" />
+                </div>
+                <button type="submit">{"Post"}</button>
+            </form>
+        }
+    } else {
+        html! {
+            <h2 class="posting-form-show" onclick={move |_| show_form.set(true)}>{"[Start a New Thread]"}</h2>
+        }
+    }
+}
 
 #[derive(Properties, PartialEq)]
 struct ThreadPostProps {
@@ -52,6 +94,7 @@ pub fn board_page(BoardPageProps { slug }: &BoardPageProps) -> Html {
                 <h1>{display_text}</h1>
                 <h2>{board.name.clone()}</h2>
                 <p>{board.description.clone()}</p>
+                <PostingForm board={board.clone()} />
                 {if board.threads.len() > 0 {
                     board.threads.iter().map(|thread| {
                         html! { <ThreadPost thread={thread.clone()} /> }

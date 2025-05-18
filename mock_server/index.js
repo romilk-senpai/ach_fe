c80225d96fabcc132deee7bb9d7e9a0c683ed2cd
ivan <ivanvaneck2000@gmail.com>
2025-05-19 01:14:23 +0300

    fix mock server

--- mock_server/index.js
+++ mock_server/index.js
@@ -4,35 +4,46 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
 
   // Handle /boards/:slug route
-  if (req.url.startsWith('/board?slug=')) {
-    const slug = req.url.split('=')[1];
-    res.end(JSON.stringify({
-      category_id: 1,
-      id: 1,
-      name: "Random",
-      slug: slug,
-      description: "Random board",
-      threads: [
-        {
-          id: "1",
-          num: 1,
-          name: "~sonter-rabryd",
-          subject: "Test Thread",
-          content: "This is a test thread",
-          timestamp: 1716150000,
-          board: "a"
-        }
-      ]
-    }));
+  if (req.url.startsWith("/board?slug=")) {
+    const slug = req.url.split("=")[1];
+    res.end(
+      JSON.stringify({
+        categoryName: "Misc.",
+        id: 1,
+        name: "Random",
+        slug: slug,
+        description: "Random board",
+        threads: [
+          {
+            lastReplies: [
+              {
+                id: 1,
+                author: "ayanokojimode",
+                subject: "topic 1",
+                content: "sample post 1",
+                createdAt: "2025-05-18T15:41:20.936326Z",
+              },
+            ],
+            opPost: {
+              id: 1,
+              author: "~sonter-rabryd",
+              subject: "Test Thread",
+              content: "This is a test thread",
+              createdAt: "2025-05-18T15:41:20.936326Z",
+            },
+          },
+        ],
+      })
+    );
     return;
   }
 
   // Default route - return boards list
   res.end(
     JSON.stringify([
-      { categoryId: 1, id: 1, name: "Anime & Manga", slug: "a" },
-      { categoryId: 2, id: 2, name: "Random", slug: "b" },
-      { categoryId: 2, id: 3, name: "ROBOT9001", slug: "r9k" },
+      { categoryName: "Japanese Culture", id: 1, name: "Anime & Manga", slug: "a" },
+      { categoryName: "Misc.", id: 2, name: "Random", slug: "b" },
+      { categoryName: "Misc.", id: 3, name: "ROBOT9001", slug: "r9k" },
     ])
   );
 });
